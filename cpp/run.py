eee128679cb413bb55362bb926d13a3553906271
LuizFXavier <zap422238@gmail.com>
2026-05-25 00:21:17 -0400

    style: Use pattern from other run file

--- cpp/run.py
+++ cpp/run.py
@@ -1,18 +1,21 @@
 import subprocess
 import os
 
-base_in = "../inputs/parquet/"
-base_out = "./output/"
+# base_in = "../inputs/parquet/"
+# base_out = "./output/"
+ 
+base_in = "/app/inputs/parquet/"
+base_out = "/app/output/full_explore/"
 
-
-inputs = ["10nds", "100nds", "1_000nds"]
+executable_path = "/app/out/build/docker-sycl/dimna/di_mna" 
+inputs = ["10_nds", "100nds", "1_000nds", "10_000nds"]
 
 runs = "1"
 
-alfas = ["1", "2", "3"]
-betas = ["500", "1000", "5000"]
+alfas = ["2"]
+betas = ["50000"]
 
-for input in inputs[2:3]:
+for input in inputs:
     for a in alfas:
         for b in betas:
 
@@ -22,7 +25,7 @@ for input in inputs[2:3]:
             if not os.path.exists(out_path):
                 os.makedirs(out_path)
 
-            subprocess.run(["./build/canon_dimna/canon_di_mna",
+            subprocess.run([executable_path,
                             "--in", base_in + input,
                             "--out", out_path,
                             "--runs", runs,
